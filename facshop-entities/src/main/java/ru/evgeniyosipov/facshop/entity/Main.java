26d23fd7d4a5f9b296ea275ab4ef5bbc6464a0dc
AnnaEleeva <anna_eleeva@yahoo.com>
2021-02-22 19:58:29 +0500

    add all2

---
+++ facshop-entities/src/main/java/ru/evgeniyosipov/facshop/entity/Main.java
@@ -0,0 +1,30 @@
+package ru.evgeniyosipov.facshop.entity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Base64;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(encoder("C:\\Users\\Anna\\Downloads\\facshop\\facshop-resources\\src\\main\\resources\\META-INF\\resources\\img\\category_icon.png").toLowerCase());
+
+    }
+
+    public static String encoder(String imagePath) {
+        String base64Image = "";
+        File file = new File(imagePath);
+        try (FileInputStream imageInFile = new FileInputStream(file)) {
+            // Reading a Image file from file system
+            byte imageData[] = new byte[(int) file.length()];
+            imageInFile.read(imageData);
+            base64Image = Base64.getEncoder().encodeToString(imageData);
+        } catch (FileNotFoundException e) {
+            System.out.println("Image not found" + e);
+        } catch (IOException ioe) {
+            System.out.println("Exception while reading the Image " + ioe);
+        }
+        return base64Image;
+    }
+}
